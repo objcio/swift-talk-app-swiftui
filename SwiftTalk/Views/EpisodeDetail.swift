813aba7b90d95856d4dea01ca04ad01ade6f20a9
Chris Eidhof <chris@eidhof.nl>
2019-07-04 16:17:35 +0200

    Fix date decoding

---
+++ SwiftTalk/Views/EpisodeDetail.swift
@@ -0,0 +1,23 @@
+//
+//  EpisodeDetail.swift
+//  SwiftTalk
+//
+//  Created by Chris Eidhof on 04.07.19.
+//  Copyright © 2019 Chris Eidhof. All rights reserved.
+//
+
+import SwiftUI
+
+struct EpisodeDetail : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct EpisodeDetail_Previews : PreviewProvider {
+    static var previews: some View {
+        EpisodeDetail()
+    }
+}
+#endif
---
+++ SwiftTalk/Views/PlayerView.swift
@@ -0,0 +1,23 @@
+//
+//  PlayerView.swift
+//  SwiftTalk
+//
+//  Created by Chris Eidhof on 04.07.19.
+//  Copyright © 2019 Chris Eidhof. All rights reserved.
+//
+
+import SwiftUI
+
+struct PlayerView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct PlayerView_Previews : PreviewProvider {
+    static var previews: some View {
+        PlayerView()
+    }
+}
+#endif
